aae3530625529901b060b3a5d3185d75fcf425bc
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-01-03 14:59:36 +0800

    sample database

--- raspberry/database_handler.py
+++ raspberry/database_handler.py
@@ -3,7 +3,12 @@ import json
 import threading
 import time
 from copy import deepcopy
+import os
 
 class DataHandler(threading.Thread):
     
-    pass
+    images_path = os.path.join(os.path.dirname(__file__), "images")
+    
+    nurses_path = os.path.join(os.path.dirname(__file__), "databases", "nurses.json")
+    patients_path = os.path.join(os.path.dirname(__file__), "databases", "patients.json")
+    
