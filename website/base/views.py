1b74fa183710af57038f67dc2c8c6789ad141c98
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-01-16 01:32:44 +0800

    create a sample login

--- website/base/views.py
+++ website/base/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def index(request):
     return render(request, 'homepage.html')
+
+def login_user(request):
+    return JsonResponse({'error': 'You must be authenticated to view this page.'})
