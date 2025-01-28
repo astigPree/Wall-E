00a8c0b60771eeae697c68b3cb3fd627d30b2c70
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-01-28 22:19:09 +0800

    update git ignore

--- raspberry/script_tester.py
+++ raspberry/script_tester.py
@@ -1,12 +1,11 @@
 from my_tools import *
 import time
+import rules
  
-data = send_post_request()
-if data:
-    print(f"Received data: {data}")
-
-import datetime
-
+# data = send_post_request()
+# if data:
+#     print(f"Received data: {data}")
+ 
 data = {
     'patients': {
         '8': {
@@ -44,3 +43,17 @@ data = {
         }
     }
 }
+
+
+
+from aitextgen import aitextgen
+
+# Without any parameters, aitextgen() will download, cache, and load the 124M GPT-2 "small" model
+ai = aitextgen()
+
+ai.generate()
+ai.generate(n=3, max_length=100)
+ai.generate(n=3, prompt="I believe in unicorns because", max_length=100)
+ai.generate_to_file(n=10, prompt="I believe in unicorns because", max_length=100, temperature=1.2)
+
+
