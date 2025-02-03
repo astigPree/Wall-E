25951077bd9c063c56621f4b26df0040360ef163
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-02-04 00:25:17 +0800

    fix error handler

--- arduino/raspberry_connection/raspberry_connection.ino
+++ arduino/raspberry_connection/raspberry_connection.ino
@@ -1,11 +1,12 @@
+#include <Servo.h>
 
-
-
+Servo lock;  // Create a servo object
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.setTimeout(1);
+  lock.attach(4); // Attach the servo to pin 4
 }
 
 void loop() { 
@@ -16,7 +17,18 @@ void loop() {
     // The '\n' character is used to mark the end of the incoming data. The readStringUntil() function reads all characters until it encounters a newline character.
     // The readStringUntil() function returns the entire string received, excluding the newline character.
     String data = Serial.readStringUntil('\n'); // Read the incoming data until a newline character
- 
+
+    if (data == "LOCK"){
+      Serial.println("Locking System...");
+      for (int pos = 0; pos <= 180; pos += 1) { // Move servo from 0 to 180 degrees
+        lock.write(pos); // Tell servo to go to position in variable 'pos'
+        delay(15); // Wait 15 ms for the servo to reach the position
+      }
+      for (int pos = 180; pos >= 0; pos -= 1) { // Move servo from 180 to 0 degrees
+        lock.write(pos); // Tell servo to go to position in variable 'pos'
+        delay(15); // Wait 15 ms for the servo to reach the position
+      }
+    }
 
     if (data == "B") { 
       Serial.println("Biogesic Dropping...");
