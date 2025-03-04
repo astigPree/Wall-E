7b7257482bce42f2c2e7bdb03cccc851a42db6c8
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-03-04 22:16:48 +0800

    test speech

--- raspberry/script_tester.py
+++ raspberry/script_tester.py
@@ -1,48 +1,53 @@
 from my_tools import *
 import time
 import rules
- 
+
+from voice_utils import VoiceUtils
+
+voice = VoiceUtils()
+
+voice.speak("Hello There! Could you repeat your request because i did not understand the message")  
 # data = send_post_request()
 # if data:
 #     print(f"Received data: {data}")
  
-data = {
-    'patients': {
-        '8': {
-            'id': 8,
-            'name': 'Arthur A. Arthur',
-            'first_name': 'Arthur',
-            'last_name': 'Arthur',
-            'middle_name': 'Arthur',
-            'date_added': '2025-01-19 06:16:02',
-            'face': '/media/patient_faces/patient_image_j5Gr6Tu.jpg'
-        }
-    },
-    'nurses': {
-        '3': {
-            'id': 3,
-            'name': 'Joy T. Tarucan',
-            'first_name': 'Joy',
-            'last_name': 'Tarucan',
-            'middle_name': 'Tambak',
-            'date_added': '2025-01-19 07:00:38',
-            'face': '/media/nurse_faces/nurse_image.jpg'
-        }
-    },
-    'schedules': {
-        '9': {
-            'id': 9,
-            'nurse': None,
-            'patient': 8,
-            'created_at': '2025-01-19 00:00:00',
-            'pill': 'Citirizine',
-            'is_daily': False,
-            'is_medication_taken': False,
-            'set_date': '2025-01-15',
-            'set_time': '12:38'
-        }
-    }
-}
+# data = {
+#     'patients': {
+#         '8': {
+#             'id': 8,
+#             'name': 'Arthur A. Arthur',
+#             'first_name': 'Arthur',
+#             'last_name': 'Arthur',
+#             'middle_name': 'Arthur',
+#             'date_added': '2025-01-19 06:16:02',
+#             'face': '/media/patient_faces/patient_image_j5Gr6Tu.jpg'
+#         }
+#     },
+#     'nurses': {
+#         '3': {
+#             'id': 3,
+#             'name': 'Joy T. Tarucan',
+#             'first_name': 'Joy',
+#             'last_name': 'Tarucan',
+#             'middle_name': 'Tambak',
+#             'date_added': '2025-01-19 07:00:38',
+#             'face': '/media/nurse_faces/nurse_image.jpg'
+#         }
+#     },
+#     'schedules': {
+#         '9': {
+#             'id': 9,
+#             'nurse': None,
+#             'patient': 8,
+#             'created_at': '2025-01-19 00:00:00',
+#             'pill': 'Citirizine',
+#             'is_daily': False,
+#             'is_medication_taken': False,
+#             'set_date': '2025-01-15',
+#             'set_time': '12:38'
+#         }
+#     }
+# }
 
 
 # from aitextgen import aitextgen
