901f59d5b61e6e53bcd0735dc8ecfe37f443dead
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-03-26 21:19:45 +0800

    for debugging

--- raspberry/main.py
+++ raspberry/main.py
@@ -277,18 +277,18 @@ def main():
 
 
 if __name__ == '__main__': 
-    generated_response = brain.generate_response(rules.rules_for_introduction)
-    decided_command : dict = text_to_dictionary(generated_response)
-    if not decided_command:
-        generated_response = brain.generate_cohere_response(rules.rules_for_introduction , rules.rules_for_introduction)
-        print("Generated response by cohere: ", generated_response)
-        decided_command : dict = text_to_dictionary(generated_response)
-        print("Generated response by cohere: ", decided_command)
-    if isinstance(decided_command, dict):
-        introduction = decided_command.get("message" , "Hello, I am Well-E, your advanced healthcare assistant. I am here to ensure you take the right dosage of your medication at the correct time, monitor your body temperature for your well-being, and securely recognize you using facial recognition. You can interact with me easily through voice commands, and I automate several healthcare and patient management tasks to make your life smoother. How may I assist you today?")
-    else:
-        introduction = "Hello, I am Well-E, your advanced healthcare assistant. I am here to ensure you take the right dosage of your medication at the correct time, monitor your body temperature for your well-being, and securely recognize you using facial recognition. You can interact with me easily through voice commands, and I automate several healthcare and patient management tasks to make your life smoother. How may I assist you today?"
-    voice.speak(introduction)
+    # generated_response = brain.generate_response(rules.rules_for_introduction)
+    # decided_command : dict = text_to_dictionary(generated_response)
+    # if not decided_command:
+    #     generated_response = brain.generate_cohere_response(rules.rules_for_introduction , rules.rules_for_introduction)
+    #     print("Generated response by cohere: ", generated_response)
+    #     decided_command : dict = text_to_dictionary(generated_response)
+    #     print("Generated response by cohere: ", decided_command)
+    # if isinstance(decided_command, dict):
+    #     introduction = decided_command.get("message" , "Hello, I am Well-E, your advanced healthcare assistant. I am here to ensure you take the right dosage of your medication at the correct time, monitor your body temperature for your well-being, and securely recognize you using facial recognition. You can interact with me easily through voice commands, and I automate several healthcare and patient management tasks to make your life smoother. How may I assist you today?")
+    # else:
+    #     introduction = "Hello, I am Well-E, your advanced healthcare assistant. I am here to ensure you take the right dosage of your medication at the correct time, monitor your body temperature for your well-being, and securely recognize you using facial recognition. You can interact with me easily through voice commands, and I automate several healthcare and patient management tasks to make your life smoother. How may I assist you today?"
+    # voice.speak(introduction)
     threading.Thread(target=start_listening).start() # start listening in a separate thread
     try:
         while not event.close_down:
