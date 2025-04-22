9aa4217273b131ef1b2490147fdf8832084ca30e
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-04-22 13:06:01 +0800

    remove checking

--- raspberry/speech_recognition_utils.py
+++ raspberry/speech_recognition_utils.py
@@ -7,7 +7,7 @@ class SpeechRecognitionUtils:
         self.r.energy_threshold = 500
         self.r.pause_threshold = 1.5
         self.mic_index = self.get_usable_mic_index()  # Auto-select usable mic
-        self.mic_ready = self.test_microphone()  # Test mic once & store result
+        # self.mic_ready = self.test_microphone()  # Test mic once & store result
 
     def get_usable_mic_index(self):
         """Finds the first available microphone index and returns it."""
@@ -26,27 +26,27 @@ class SpeechRecognitionUtils:
             return None
 
 
-    def test_microphone(self):
-        """Tests if the microphone is working once on startup."""
-        if self.mic_index is None:
-            print("Error: No microphone available.")
-            return False
+    # def test_microphone(self):
+    #     """Tests if the microphone is working once on startup."""
+    #     if self.mic_index is None:
+    #         print("Error: No microphone available.")
+    #         return False
 
-        try:
-            with sr.Microphone(device_index=self.mic_index) as source:
-                print(f"Testing Microphone: {self.mic_index} ({sr.Microphone.list_microphone_names()[self.mic_index]})")
-                self.r.adjust_for_ambient_noise(source)
-                print("Say something to test...")
-                audio_text = self.r.listen(source, timeout=5)  # Set timeout to avoid indefinite waiting
-                print("Microphone is working!")
-                return True
-        except Exception as e:
-            print(f"Microphone test failed: {e}")
+    #     try:
+    #         with sr.Microphone(device_index=self.mic_index) as source:
+    #             print(f"Testing Microphone: {self.mic_index} ({sr.Microphone.list_microphone_names()[self.mic_index]})")
+    #             self.r.adjust_for_ambient_noise(source)
+    #             print("Say something to test...")
+    #             audio_text = self.r.listen(source, timeout=5)  # Set timeout to avoid indefinite waiting
+    #             print("Microphone is working!")
+    #             return True
+    #     except Exception as e:
+    #         print(f"Microphone test failed: {e}")
             return False
 
     def recognize_speech(self):
         """Recognizes speech only if the microphone has been successfully tested."""
-        if not self.mic_ready:
+        if not self.mic_index:
             print("Skipping recognition due to microphone issue.")
             return None
 
