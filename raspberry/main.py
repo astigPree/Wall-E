4d48c9226482f0501729683a059ae2312fabed76
MakieTech <121760681+astigPree@users.noreply.github.com>
2025-05-03 15:45:09 +0800

    introductions

--- raspberry/main.py
+++ raspberry/main.py
@@ -536,15 +536,15 @@ def main():
         voice.speak("I have finished serving all medications. Im available to your inquiries")
 
 if __name__ == '__main__': 
-    # generated_response = brain.generate_cohere_response(rules.rules_for_introduction , rules.rules_for_introduction)
-    # print("Generated response by cohere: ", generated_response)
-    # decided_command : dict = text_to_dictionary(generated_response)
-    # print("Generated response by cohere: ", decided_command)
-    # if isinstance(decided_command, dict):
-    #     introduction = decided_command.get("message" , "Hello, I am Stuart, your advanced healthcare assistant. I am here to ensure you take the right dosage of your medication at the correct time, monitor your body temperature for your well-being, and securely recognize you using facial recognition. You can interact with me easily through voice commands, and I automate several healthcare and patient management tasks to make your life smoother. How may I assist you today?")
-    # else:
-    #     introduction = "Hello, I am Stuart, your advanced healthcare assistant. I am here to ensure you take the right dosage of your medication at the correct time, monitor your body temperature for your well-being, and securely recognize you using facial recognition. You can interact with me easily through voice commands, and I automate several healthcare and patient management tasks to make your life smoother. How may I assist you today?"
-    # voice.speak(introduction)
+    generated_response = brain.generate_cohere_response(rules.rules_for_introduction , rules.rules_for_introduction)
+    print("Generated response by cohere: ", generated_response)
+    decided_command : dict = text_to_dictionary(generated_response)
+    print("Generated response by cohere: ", decided_command)
+    if isinstance(decided_command, dict):
+        introduction = decided_command.get("message" , "Hello, I am Stuart, your advanced healthcare assistant. I am here to ensure you take the right dosage of your medication at the correct time, monitor your body temperature for your well-being, and securely recognize you using facial recognition. You can interact with me easily through voice commands, and I automate several healthcare and patient management tasks to make your life smoother. How may I assist you today?")
+    else:
+        introduction = "Hello, I am Stuart, your advanced healthcare assistant. I am here to ensure you take the right dosage of your medication at the correct time, monitor your body temperature for your well-being, and securely recognize you using facial recognition. You can interact with me easily through voice commands, and I automate several healthcare and patient management tasks to make your life smoother. How may I assist you today?"
+    voice.speak(introduction)
     time.sleep(1)
     listening_thread = threading.Thread(target=start_listening)
     listening_thread.start() # start listening in a separate thread
